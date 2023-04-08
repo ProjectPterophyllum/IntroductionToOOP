bc4c2955370ffd68c776bbd2c1dbfc16e9015657
Alexey Bolshakov <bad2600@outlook.com>
2023-04-08 11:00:12 +0300

    UPD

--- IntroductionToOOP/main.cpp
+++ IntroductionToOOP/main.cpp
@@ -1,16 +1,23 @@
 ﻿#include <iostream>
 using namespace std;
+
 struct Point
 {
 	double x;
 	double y;
+	double z;
 };
+
 void main()
 {
 	setlocale(LC_ALL, "");
 	//type name
 	int a; //Объявление переменной а, типа int
 	Point A; //Объявление переменной А, типа Point. Объявление объекта А структуры Point, создание экземпляра А структура Point.
-
-
+	A.x = 2;
+	A.y = 3;
+	cout << A.x << "\t" << A.y << endl;
+	cout << sizeof(A) << endl;
+	Point* pA = &A;
+	cout << pA->x << "\t" << pA->y << endl;
 }
