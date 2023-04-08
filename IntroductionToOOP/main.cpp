0dd429fc1dabae51766e400a472db27d10f31c4b
Alexey Bolshakov <bad2600@outlook.com>
2023-04-08 13:22:50 +0300

    UPD

--- IntroductionToOOP/main.cpp
+++ IntroductionToOOP/main.cpp
@@ -7,6 +7,66 @@ class Point
 	double y;
 	double z;
 public:
+	double get_x() const
+	{
+		return x;
+	}
+	double get_y() const
+	{
+		return y;
+	}
+	double get_z() const
+	{
+		return z;
+	}
+	void set_x(double x)
+	{
+		this->x = x;
+	}
+	void set_y(double y)
+	{
+		this->y = y;
+	}
+	void set_z(double z)
+	{
+		this->z = z;
+	}
+	//Constructor
+	Point()
+	{
+		x = y = z = 0;
+		cout << "DefaultConstructor: \t" << this << endl;
+	}
+	Point(double x)
+	{
+		this->x = x;
+		this->y = 0;
+		this->z = 0;
+		cout << "1ArgContructor: " << endl;
+	}
+	Point(double x, double y)
+	{
+		this->x = x;
+		this->y = y;
+		this->z = 0;
+		cout << "2ArgContructor: " << endl;
+	}
+	Point(double x, double y, double z)
+	{
+		this->x = x;
+		this->y = y;
+		this->z = z;
+		cout << "3ArgContructor: " << endl;
+	}
+	~Point()
+	{
+		cout << "Destructor: \t" << this << endl;
+	}
+	//Method
+	void print()const
+	{
+		cout << "X = " << x << "\tY = " << y << "\tZ = " << z << endl;
+	}
 };
 //#define STRUCT_POINT
 void main()
@@ -25,5 +85,13 @@ void main()
 	Point* pA = &A;
 	cout << pA->x << "\t" << pA->y << endl;
 #endif // STRUCT_POINT
-
+	Point A;
+	//A.set_x(2);
+	//A.set_y(3);
+	//A.set_z(5);
+	A.print();
+	Point B(5, 0);
+	B.print();
+	Point C(1, 2, 3);
+	C.print();
 }
