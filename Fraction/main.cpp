f66d9eeea1b9c2b669c4028d7fc47c1cc7284b6f
Alexey Bolshakov <bad2600@outlook.com>
2023-04-20 01:49:33 +0300

    UPD

--- Fraction/main.cpp
+++ Fraction/main.cpp
@@ -1,58 +1,84 @@
 ﻿#include <iostream>
 using namespace std;
 
+//#define DEBUG
+
 class Fraction
 {
 private:
-	double integer;//Целая часть дроби
-	double numerator;//Числитель дроби
-	double denumerator;//Знаменатель дроби
+	int integer;//Целая часть дроби
+	int numerator;//Числитель дроби
+	int denumerator;//Знаменатель дроби
 public:
 	//            get-set методы
-	double get_int()const
+	int get_int()const
 	{
 		return integer;
 	}
-	double get_num()const
+	int get_num()const
 	{
 		return numerator;
 	}
-	double get_denum()const
+	int get_denum()const
 	{
 		return denumerator;
 	}
-	void set_int(double integer)
+	void set_int(int integer)
 	{
 		this->integer = integer;
 	}
-	void set_num(double numerator)
+	void set_num(int numerator)
 	{
 		this->numerator = numerator;
 	}
-	void set_denum(double denumerator)
+	void set_denum(int denumerator)
 	{
-		this->denumerator = denumerator;
+		denumerator != 0 ? this->denumerator = denumerator : this->denumerator = 1;
 	}
 	//             Constructor
-	Fraction(double numerator, double denumerator)
+	Fraction()
+	{
+		integer = 0;
+		numerator = 0;
+		denumerator = 1;
+	}
+	Fraction(int numerator, int denumerator)
 	{
 		integer = 0;
 		this->numerator = numerator;
-		denumerator != 0 ? this->denumerator = denumerator : this->denumerator = 1;
+		set_denum(denumerator);
+#ifdef DEBUG
 		cout << "Num / Denum constructor was used" << endl;
+#endif // DEBUG
 	}
-	Fraction(double integer, double numerator, double denumerator)
+	Fraction(int integer, int numerator, int denumerator)
 	{
 		this->integer = integer;
 		this->numerator = numerator;
-		denumerator != 0 ? this->denumerator = denumerator : this->denumerator = 1;
+		set_denum(denumerator);
+#ifdef DEBUG
 		cout << "Integer + Num / Denum constructor was used" << endl;
+#endif // DEBUG
+	}
+	//Конструктор копирования
+	Fraction(const Fraction& other)
+	{
+		this->integer = other.integer;
+		this->numerator = other.numerator;
+		this->denumerator = other.denumerator;
+#ifdef DEBUG
+		cout << "CopyConstructor:\t" << this << endl;
+#endif // DEBUG
+
 	}
 	~Fraction()
 	{
-		cout << "Destructor: \t" << this;
+#ifdef DEBUG
+		cout << "Destructor: \t" << this << endl;
+#endif // DEBUG
 	}
 	//             Method:
+#ifdef DEBUG
 	void Print() const
 	{
 		if (integer != 0)
@@ -61,10 +87,141 @@ public:
 			cout << numerator << "/" << denumerator;
 		cout << endl;
 	}
+#endif // DEBUG
+	//Упрощение дроби
+	void simplify()
+	{
+		int gcd = calculateGCD(numerator, denumerator);
+		numerator /= gcd;
+		denumerator /= gcd;
+	}
+	//Преобразование из неправильной в смешаную
+	void conversion()
+	{
+		if (!properFraction())
+		{
+			integer += numerator / denumerator;
+			numerator = numerator % denumerator;
+		}
+	}
+	//Из смешаной в неправильную
+	void reverseconversion()
+	{
+		if (integer > 0)
+		{
+			numerator += integer * denumerator;
+			integer = 0;
+		}
+	}
+#ifdef DEBUG
+	//Может сказать какая это дробь.
+	string whichFraction() const
+	{
+		string result;
+		if (integer > 0)
+			return result = "Дробь смешанная";
+		else if (properFraction())
+			return result = "Дробь правильная";
+		else
+			return result = "Дробь неправильная";
+	}
+#endif // DEBUG
+
+	//              Operators
+	//Оператор +
+	Fraction& operator+(const Fraction& other)
+	{
+		if (this->denumerator != other.denumerator)
+		{
+			int lcm = calculateLCM(this->denumerator, other.denumerator);
+			this->numerator = (this->numerator * lcm / this->denumerator) + (other.numerator * lcm / other.denumerator);
+			this->denumerator = (this->denumerator * lcm / this->denumerator);
+		}
+		else
+			this->numerator += other.numerator;
+		this->integer += other.integer;
+		simplify();
+		conversion();
+		return *this;
+	}
+	//Оператор -
+	Fraction& operator-(const Fraction& other)
+	{
+		if (this->denumerator != other.denumerator)
+		{
+			int lcm = calculateLCM(this->denumerator, other.denumerator);
+			this->numerator = (this->numerator * lcm / this->denumerator) - (other.numerator * lcm / other.denumerator);
+			this->denumerator = (this->denumerator * lcm / this->denumerator);
+		}
+		else
+			this->numerator -= other.numerator;
+		this->integer -= other.integer;
+		simplify();
+		conversion();
+		return *this;
+	}
+	//Оператор *
+	Fraction& operator*(const Fraction& other)
+	{
+		this->numerator = (this->numerator + this->integer * this->denumerator) * (other.numerator + other.integer * other.denumerator);
+		this->denumerator = this->denumerator * other.denumerator;
+		simplify();
+		conversion();
+		return *this;
+	}
+	//Оператор /
+	Fraction& operator/(const Fraction& other)
+	{
+		this->numerator = (this->numerator + this->integer * this->denumerator) * other.denumerator;
+		this->denumerator = this->denumerator * (other.numerator + other.integer * other.denumerator);
+		simplify();
+		conversion();
+		return *this;
+	}
+	//In-out
+	friend istream& operator>>(istream& in, Fraction& name)
+	{
+		char slash = '/';
+		char space = '|';
+		in >> name.integer >> space >> name.numerator >> slash >> name.denumerator;
+		return in;
+	}
+	friend ostream& operator<<(ostream& out, const Fraction& name)
+	{
+		if (name.integer != 0)
+			out << name.integer << "|" << name.numerator << "/" << name.denumerator;
+		else
+			out << name.numerator << "/" << name.denumerator;
+		return out;
+	}
+private:
+	//Проверяет правильная дробь или неправильная
+	bool properFraction()const
+	{
+		return numerator < denumerator ? true : false;
+	}
+	//НОД
+	int calculateGCD(const int num, const int denum)const {
+		if (denum == 0) {
+			return num;
+		}
+		return calculateGCD(denum, num % denum);
+	}
+	//НОК
+	int calculateLCM(const int num, const int denum)const
+	{
+		int gcd = calculateGCD(num, denum);
+		return (num * denum) / gcd;
+	}
 };
 void main()
 {
 	setlocale(LC_ALL, "");
-	Fraction A(3, 10, 3);
-	A.Print();
+	Fraction C;
+	Fraction A(13, 8);
+	Fraction B(1, 1, 8);
+	A = A / B;
+	cout << A << endl;
+	cin >> C;
+	cout << C;
 }
