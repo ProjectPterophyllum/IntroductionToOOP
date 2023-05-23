bac2915f552f37a02295d17c7b32d3b98719ae62
Alexey Bolshakov <bad2600@outlook.com>
2023-05-23 23:27:12 +0300

    UPD

--- Matrix/main.cpp
+++ Matrix/main.cpp
@@ -1,14 +1,31 @@
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
 #define DEBUG
 class Matrix;
-
+Matrix& operator+(const Matrix& Left, const Matrix& Right);
 class Matrix
 {
 	int ROWS;
 	int COLS;
 	int** mtr;
 public:
+	int get_rows() const
+	{
+		return ROWS;
+	}
+
+	int get_cols() const
+	{
+		return COLS;
+	}
+	int** get_mtr() const
+	{
+		return mtr;
+	}
+	int** get_mtr()
+	{
+		return mtr;
+	}
 	//				Constructors:
 	explicit Matrix(int r = 5, int c = 5)
 	{
@@ -20,24 +37,27 @@ public:
 			mtr[i] = new int[COLS] {};
 		}
 #ifdef DEBUG
-		cout << "Constructor:\t" << endl;
+		cout << "Constructor:\t" << this << endl;
 #endif // DEBUG
 	}
 	//				Destructor:
 	~Matrix()
 	{
+		/*Я видел такое, что вам, людям, и не снилось. Атакующие корабли, пылающие над Орионом; Лучи Си, разрезающие мрак у ворот Тангейзера. 
+		Все эти мгновения затеряются во времени, как... слёзы в дожде... Пришло время умирать.*/
 		for (int i = 0; i < ROWS; i++)
 		{
 			delete[] mtr[i];
 		}
 		delete[] this->mtr;
 #ifdef DEBUG
-		cout << "Destructor:\t" << endl;
+		cout << "Destructor:\t" << this << endl;
 #endif // DEBUG
 	}
 	//				Methods:
 	void Print() const
 	{
+		cout << "Matrix: " << this << endl;
 		for (int i = 0; i < ROWS; i++)
 		{
 			for (int j = 0; j < COLS; j++)
@@ -47,10 +67,48 @@ public:
 			cout << endl;
 		}
 	}
+	void FillRandom(int start, int end)
+	{
+		for (int i = 0; i < ROWS; i++)
+		{
+			for (int j = 0; j < COLS; j++)
+			{
+				mtr[i][j] = start + rand() % (end - start + 1);
+			}
+		}
+	}
+	void Fillcin()
+	{
+		for (int i = 0; i < ROWS; i++)
+		{
+			for (int j = 0; j < COLS; j++)
+			{
+				cin >> mtr[i][j];
+			}
+			cout << endl;
+		}
+	}
+	//				Operators:
 };
 void main()
 {
 	setlocale(LC_ALL, "");
 	Matrix A;
+	Matrix B;
 	A.Print();
+	//A.Fillcin();
+	//A.Print();
+	B.FillRandom(1, 25);
+	B.Print();
+}
+Matrix& operator+(const Matrix& Left, const Matrix& Right)
+{
+	Matrix tmp;
+	if (Left.get_rows() == Right.get_rows() && Left.get_cols() == Right.get_cols())
+		for (int i = 0; i < Left.get_rows(); i++)
+			for (int j = 0; j < Left.get_cols(); j++)
+				tmp.get_mtr()[i][j] = Left.get_mtr()[i][j] + Right.get_mtr()[i][j];
+	else
+		cout << "Матрицы разные по размеру, сложение невозможно" << endl;
+	return tmp;
 }
