7e6408e20c6bf5bc65ad28f093c21dbb7b60237f
Alexey Bolshakov <bad2600@outlook.com>
2023-05-28 16:04:41 +0300

    UPD

--- String/main.cpp
+++ String/main.cpp
@@ -1,5 +1,111 @@
-﻿#include "stdafx.h"
-#include "String.h"
+﻿#include <iostream>
+using namespace std;
+#define HOME_WORK
+#define DEBUG
+//Объявление класса
+class String;
+//Объявление операторов
+std::ostream& operator<<(ostream& out, const String& name);
+String operator+(const String& Left, const String& Right);
+
+class String
+{
+	int size; //Размер строки в байтах
+	char* str; //Адрес строки в динамической памяти
+public:
+	//get-set Методы
+	int get_size()const
+	{
+		return size;
+	}
+	char* get_str()const
+	{
+		return str;
+	}
+	char* get_str()
+	{
+		return str;
+	}
+	//			Constructors
+	explicit String(int size = 80) :size(size), str(new char[size] {})
+	{
+#ifdef DEBUG
+		cout << "Constructor:\t" << this << endl;
+#endif // DEBUG
+
+	}
+	String(const char string[]) :size((int)strlen(string) + 1), str(new char[size] {})
+	{
+		for (int i = 0; i < size; i++) str[i] = string[i];
+#ifdef DEBUG
+		cout << "Constructor:\t" << this << endl;
+#endif // DEBUG
+	}
+	String(const String& other) :size(other.size), str(new char[size] {})
+	{
+		for (int i = 0; i < size; i++) str[i] = other.str[i];
+#ifdef DEBUG
+		cout << "CopyConstructor:" << this << endl;
+#endif // DEBUG
+
+	}
+	String(String&& other)noexcept :size(other.size), str(other.str)
+	{
+		//Shallow Copy
+		other.size = 0;
+		other.str = nullptr;
+		cout << "MoveConstructor:\t" << this << endl;
+	}
+	//			Destructor
+	~String()
+	{
+		delete[] this->str;
+#ifdef DEBUG
+		cout << "Destructor:\t" << this << endl;
+#endif // DEBUG
+
+	}
+	//			Methods
+	void print()const
+	{
+		cout << "Size: " << size << endl;
+		cout << "Str: " << str << endl;
+	}
+	//			Operators
+	String& operator=(const String& other)
+	{
+		if (this->str == other.str) return *this;
+		delete[] this->str;
+		this->size = other.size;
+		this->str = new char[size] {};
+		for (int i = 0; i < size; i++) str[i] = other.str[i];
+		cout << "CopyAssignment:\t" << this << endl;
+		return *this;
+	}
+	String& operator=(String&& other) noexcept
+	{
+		if (this->str == other.str) return *this;
+		delete[]this->str;
+		size = other.size;
+		str = other.str;
+		other.size = 0;
+		other.str = nullptr;
+		cout << "MoveAssignment:\t" << this << endl;
+		return *this;
+	}
+	String& operator+=(const String& other)
+	{
+		return *this = *this + other;
+	}
+	char operator[](int index) const
+	{
+		return str[index];
+	}
+	char& operator[](int index)
+	{
+		return str[index];
+	}
+};
 //			Operators:
 
 void main()
